47944e5abe21d73e056622d0290ba4f4f50ffb06
Tim Bartsch <timbartsch.de@gmail.com>
2013-08-07 14:44:28 +0200

    Added esprima for live logs.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,39 @@
 var app = (function($, undefined){
   var module = {};
 
+  String.prototype.splice = function( idx, rem, s ) {
+    return (this.slice(0,idx) + s + this.slice(idx + Math.abs(rem)));
+  };
+
+  function traverse(object, visitor) {
+    var key, child;
+
+    visitor.call(null, object);
+    for (key in object) {
+      if (object.hasOwnProperty(key)) {
+        child = object[key];
+        if (typeof child === 'object' && child !== null) {
+          traverse(child, visitor);
+        }
+      }
+    }
+  }
+
+  function buildLogString(name, line){
+    return ";log(" + name + ", '" + name + "', " + line +");";
+  }
+
   function handleEditorChange(evt){
-    var code;
+    var code,
+        parseTree,
+        logMarks,
+        logMark,
+        logString,
+        logStringOffset,
+        i;
+
+    logMarks = [];
+    logStringOffset = 0;
 
     try {
       $("#logs").empty();
@@ -10,6 +41,27 @@ var app = (function($, undefined){
       if(code.indexOf("while") !== -1){
         $("#error-notification").text("while loops are currently not supported");
       } else {
+        parseTree = esprima.parse(code, {loc: true, range: true});
+        traverse(parseTree, function(node){
+          if(node.type === "VariableDeclaration" || node.type === "ExpressionStatement") {
+            traverse(node, function(n){
+              if(n.type === "Identifier"){
+                logMarks.push({
+                  pos: node.range[1],
+                  name: n.name,
+                  line: node.loc.end.line
+                });
+              }
+            });
+          }
+        });
+        for(i=0; i<logMarks.length; i++) {
+          logMark = logMarks[i];
+          logString = buildLogString(logMark.name, logMark.line);
+          code = code.splice(logMark.pos + logStringOffset, 0, logString);
+          logStringOffset += logString.length;
+        }
+        console.log(code);
         eval(code);
         editor.removeAllMarkers();
         $("#error-notification").text("");
@@ -32,14 +84,20 @@ var app = (function($, undefined){
 
 })(jQuery);
 
-var log = function(obj, line){
+var log = function(obj, objName, line){
 	var text,
 		  oldText,
 		  $log;
+
+  line = line -1;
 	
   if(typeof line != "number" || line < 0){
 		throw "wrong argument exceptiont";
 	}
+
+  if(typeof obj === "function"){
+    return ;
+  }
 	
   if(typeof obj === "object"){
 		text = JSON.stringify(obj);
@@ -53,7 +111,7 @@ var log = function(obj, line){
 		oldText = $log.text();
 		$log.text(oldText + "; " + text);
 	}else{
-		$("#logs").append("<div class='log' id='log-" + line +"'>"+ text +"</div>");
+		$("#logs").append("<div class='log' id='log-" + line +"'>"+ objName + ": " + text +"</div>");
 		$("#log-" + line).css("top", 16 * line + "px");
 	}
 };
